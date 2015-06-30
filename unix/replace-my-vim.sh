1124737ba0576763f295e99abc61c5bc02de9097
YunxiZhang <zhangyunxi@ramos.com.cn>
2015-06-30 10:00:57 +0800

    auto set config for linux shell
    1 add desetEx color
    2 add global gtags plugin
    3 auto set default tmux add git config file

--- unix/replace-my-vim.sh
+++ unix/replace-my-vim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 cp ./dist/ctags_lang ~/.ctags
 cp .vimrc ~/.vimrc
 cp .vimrc.plugins ~/.vimrc.plugins
@@ -7,3 +8,10 @@ cp .vimrc.local ~/.vimrc.local
 cp .vimrc.plugins.local ~/.vimrc.plugins.local
 rm -rf ~/.vim
 cp -r vimfiles ~/.vim
+
+# for zzdts
+cp zzdts/gitconfig ~/.gitconfig
+cp zzdts/tmux.conf ~/.tmux.conf
+cp zzdts/desertEx.vim ~/.vim/bundle/ex-colorschemes/colors/desertEx.vim
+mkdir -p ~/.vim/bundle/global/plugin/
+cp -f zzdts/gtags.vim  ~/.vim/bundle/global/plugin/
